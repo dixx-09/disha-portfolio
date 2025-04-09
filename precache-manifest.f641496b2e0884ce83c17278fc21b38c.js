9b9a5869c63e8d8031f521777c2d2524ef92c785
Your Name <your.email@example.com>
2025-04-09 16:53:03 +0530

    Updates

---
+++ precache-manifest.f641496b2e0884ce83c17278fc21b38c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b43bce5a626c866f223a1f263962b41",
+    "url": "/disha-portfolio/index.html"
+  },
+  {
+    "revision": "ba962d836f25c106deed",
+    "url": "/disha-portfolio/static/css/main.49ca8e89.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/disha-portfolio/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "ba962d836f25c106deed",
+    "url": "/disha-portfolio/static/js/main.d3a99210.chunk.js"
+  },
+  {
+    "revision": "95c94084908c0a77b916",
+    "url": "/disha-portfolio/static/js/runtime~main.c619962c.js"
+  }
+]);
